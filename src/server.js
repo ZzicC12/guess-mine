c86789e5d833b224d523fe8d816d3acec751367d
ZzicC12 <pyw251@gmail.com>
2021-03-08 16:44:04 +0900

    2.3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { join } from "path";
-import socketIO from "socket.io";
+import socket from "socket.io";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 4000;
@@ -8,6 +9,7 @@ const PORT = 4000;
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => res.render("home"));
 
@@ -17,4 +19,16 @@ const handleListen = () => {
 
 const server = app.listen(PORT, handleListen);
 
-const io = socketIO(server);
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", (data) =>
+    socket.broadcast.emit("receive_message", {
+      message: data.message,
+      name: socket.name || "Anon",
+    })
+  );
+  socket.on("setName", (data) => {
+    socket.name = data.name;
+  });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,15 @@
-index.js;
+const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+}
+
+function setName(name) {
+  socket.emit("setName", { name });
+}
+
+const handleReceive = (data) => {
+  console.log(data.name, ":", data.message);
+};
+
+socket.on("receive_message", handleReceive);
